b37c19076fb234989d6d12d39c486ce8deca32b9
bion howard <bion@nanohacker.net>
2017-02-28 17:29:17 -0800

    fixed nav routes

--- client/assets/angular/app.js
+++ client/assets/angular/app.js
@@ -5,12 +5,16 @@ var app = angular.module('app', ['ngRoute', 'ngMessages']);
 app.config( function ($routeProvider) {
 
   $routeProvider
-
     .when('/', {
       templateUrl: 'partials/login.html',
-      controller: 'loginController',
-      controllerAs: 'LC'
+      controller: 'loginController'
     })
+
+    .when('/dashboard', {
+      templateUrl: 'partials/dashboard.html',
+      controller: 'dashboardController'
+    })
+
     .when('/inventory', {
       templateUrl: 'partials/inventory.html',
       controller: 'inventoryController',
@@ -25,21 +29,6 @@ app.config( function ($routeProvider) {
     .when('/companies', {
       templateUrl: 'partials/companies.html',
       controller: 'companiesController'
-    })
-    .when('/add_order', {
-      templateUrl: 'partials/add_order.html',
-      controller: 'addOrderController',
-      controllerAs: 'AOC'
-    })
-    .when('/add_company', {
-      templateUrl: 'partials/add_company.html',
-      controller: 'addCompanyController',
-      controllerAs: 'ACC'
-    })
-    .when('/add_order/:company_id', {
-      templateUrl: 'partials/add_products.html',
-      controller: 'addProductsController',
-      controllerAs: 'APC'
     })
 
     .otherwise({
