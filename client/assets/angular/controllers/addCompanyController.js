c8a2bd63cb95039c8c36a78caa212142ffa34279
Nishant Patel <contact@nishant-patel.com>
2017-03-01 16:53:44 -0800

    Angular Cookies Worked

--- client/assets/angular/controllers/addCompanyController.js
+++ client/assets/angular/controllers/addCompanyController.js
@@ -1,20 +1,29 @@
 console.log("Loading Clientside addCompanyController.js");
 
-app.controller('addCompanyController', ['$scope', '$location', 'productFactory','companyFactory', function ($scope, $location, productFactory, companyFactory) {
+app.controller('addCompanyController', ['$scope', '$location', 'productFactory','companyFactory', '$cookieStore' ,function ($scope, $location, productFactory, companyFactory, $cookieStore) {
 
-  // Initialize Required Attributes
+
+  var self = this;
 
   // -------------------------------------------------------------------------
   //                            Add Required Methods
   // -------------------------------------------------------------------------
-  var self = this;
+  var CheckingUser = function () {
+    console.log("Removing User");
+    $cookieStore.remove('logged-in');
+  };
+  CheckingUser();
+
+  // -------------------------------------------------------------------------
+  //                            Create Company
+  // -------------------------------------------------------------------------
 
   this.create = function(){
     console.log('you clicked create company',$scope.company);
     companyFactory.create($scope.company,function(company){
       console.log('returned to the addCompanyController with created company',company.data);
-      $location.url('/')
-    })
-  }
+      $location.url('/');
+    });
+  };
 
 }]);
--- client/assets/angular/controllers/loginController.js
+++ client/assets/angular/controllers/loginController.js
@@ -3,13 +3,24 @@ console.log("Loading Clientside loginController.js");
 app.controller('loginController', ['$scope', '$location', 'userFactory', function ($scope, $location, userFactory) {
 
   var _this = this;
-  _this.newUser = {}
+  _this.existingUser = {};
+  _this.error_messages = [];
 
   // -------------------------------------------------------------------------
   //                            Create New User
   // -------------------------------------------------------------------------
-  _this.createUser = function () {
-    console.log(_this.newUser);
-  }
+  _this.login = function () {
+    console.log(_this.existingUser);
+    userFactory.login(_this.existingUser, function (dataFromServer) {
+      if (dataFromServer.success === false) {
+        console.log(dataFromServer.error_messages);
+        _this.success = false;
+        _this.error_messages = dataFromServer.error_messages;
+      } else {
+        _this.existingUser = {};
+        $location.url('/userdashboard');
+      }
+    });
+  };
 
 }]);
